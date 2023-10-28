570d196d2ae16e4d01f922d9787b43192c7b173f
ameer005 <ak2260650@gmail.com>
2023-10-28 19:33:34 +0000

    2022 day02

---
+++ 2022/day02/target/debug/day02.d
@@ -0,0 +1,1 @@
+/home/ameer005/documents/projects/learning/rust/dsa/advent_of_code/2022/day02/target/debug/day02: /home/ameer005/documents/projects/learning/rust/dsa/advent_of_code/2022/day02/src/main.rs
---
+++ 2022/day02/target/debug/deps/day02-ee48c5a82a0dab60.d
@@ -0,0 +1,5 @@
+/home/ameer005/documents/projects/learning/rust/dsa/advent_of_code/2022/day02/target/debug/deps/day02-ee48c5a82a0dab60.rmeta: src/main.rs
+
+/home/ameer005/documents/projects/learning/rust/dsa/advent_of_code/2022/day02/target/debug/deps/day02-ee48c5a82a0dab60.d: src/main.rs
+
+src/main.rs:
